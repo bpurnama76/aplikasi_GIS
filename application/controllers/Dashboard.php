64b37043b2c61458822b9dc7b07357bd6eac540c
saefulhuda <saefulhuda54@gmail.com>
2019-09-08 00:58:09 +0700

    setting template

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* 
+*/
+class Dashboard extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent:: __construct();
+	}
+
+	function index() 
+	{
+		$this->template->load('template','dashboard/info');
+	}
+}
---
+++ application/views/dashboard/info.php
@@ -0,0 +1,8 @@
+<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+	<div class="panel panel-teal panel-widget border-right">
+		<div class="row no-padding"><em class="fa fa-xl fa-shopping-cart color-blue"></em>
+			<div class="large">120</div>
+			<div class="text-muted">New Orders</div>
+		</div>
+	</div>
+</div>
